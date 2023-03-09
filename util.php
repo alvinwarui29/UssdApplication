244d274b79fb05261472b674506cf92223918c36
alvincanton29@gmail.com <alvincanton29@gmail.com>
2023-03-10 01:32:52 +0300

    Database connected and user class created

--- util.php
+++ util.php
@@ -1,6 +1,9 @@
 <?php
-
 class Util{
+    static $DB_NAME = "ussdsms";
+    static $DB_USER = "root";
+    static $DB_USER_PASS = "";
+    static $SERVER_NAME = "localhost";
 
     static $GO_BACK = "98";
     static $GO_TO_MAIN_MENU = "99";
